05765fcce98e99b7767558ef821d7d96e67cefb5
wangshengjin <934457719@qq.com>
2018-11-08 14:30:12 +0800

    修改隐藏虚拟按键模式

--- src/android/NBTools.java
+++ src/android/NBTools.java
@@ -69,10 +69,6 @@ public class NBTools extends CordovaPlugin {
             public void run() {
                 View decorView = cordova.getActivity().getWindow().getDecorView();
                 int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE
-                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY; // 这个是关键
                 decorView.setSystemUiVisibility(uiOptions);
                 callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, true));
